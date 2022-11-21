d9c7146d6fc9a736212e0bab60be93056a7e16f1
LukeDFlynn <118565875+LukeDFlynn@users.noreply.github.com>
2022-11-20 20:13:36 -0600

    Add files via upload

--- test_account.py
+++ test_account.py
@@ -1,4 +1,4 @@
-import pytest
+from pytest import *
 from account import *
 
 class Test:
@@ -15,18 +15,24 @@ class Test:
     def test_init(self):
         assert self.account_one.get_name() == 'John'
         assert self.account_two.get_name() == 'Jim'
+        assert self.account_one.get_balance() == approx(20.5, abs=0.001)
 
     def test_deposit(self):
         assert self.account_one.deposit(5) is True
+        assert self.account_one.deposit(0) is False
         assert self.account_one.deposit(-16) is False
         assert self.account_two.deposit(53) is True
         assert self.account_two.deposit(-4) is False
 
+        assert self.account_one.get_balance() == approx(20.5, abs=0.001)
+
     def test_withdraw(self):
         assert self.account_one.withdraw(5) is False
+        assert self.account_one.withdraw(0) is False
         assert self.account_one.withdraw(-17) is False
         assert self.account_two.withdraw(600) is False
         assert self.account_two.withdraw(-32) is False
 
+        assert self.account_one.get_balance() == approx(20.5, abs=0.001)
 
 
