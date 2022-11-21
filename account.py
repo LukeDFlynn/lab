6570aa423462001e70189821034c9531cba7f02d
LukeDFlynn <118565875+LukeDFlynn@users.noreply.github.com>
2022-11-20 19:45:21 -0600

    Add files via upload

---
+++ account.py
@@ -0,0 +1,31 @@
+class Account:
+
+    def __init__(self, name):
+        self.__account_name = name
+        self.__account_balance = 0
+
+    def deposit(self, amount):
+        '''
+        :param amount: amount
+        :return: True
+        '''
+        if amount != 0 and amount > 0:
+            self.__account_balance += amount
+        else:
+            return False
+
+    def withdraw(self, amount):
+        '''
+        :param amount: amount
+        :return: True
+        '''
+        if amount != 0 and 0 < amount <= self.__account_balance:
+            self.__account_balance -= amount
+        else:
+            return False
+
+    def get_balance(self):
+        return self.__account_balance
+
+    def get_name(self):
+        return self.__account_name
